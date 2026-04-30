e98ab9aab0f08acdcb2690e27a9e55817cfd4208
fluffykoo <oumou.camara@efrei.net>
2026-04-30 11:06:16 +0200

    add base test and fix sonar config

---
+++ MyService/src/test/java/com/example/myservice/MyServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.myservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
